61eff1f62e922b6d3c716867eb3031f3d7d14cea
ahargunyllib <billy.bpm03@gmail.com>
2026-08-18 21:32:20 +0700

    add @astrojs/sitemap integration and update package.json

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import sitemap from "@astrojs/sitemap";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://melangkah-studio.com", // TODO: we dont have a domain yet, so this is just a placeholder
+  // TODO: we dont have a domain yet, so this is just a placeholder
+  site: "https://melangkah-studio.com",
   trailingSlash: "always",
+  integrations: [sitemap()],
 });
