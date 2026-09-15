0fafacd5813dd54663a6961adf989adfe103f8d2
ahargunyllib <billy.bpm03@gmail.com>
2026-09-15 17:51:00 +0700

    fix: complete homepage SEO metadata

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,11 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [sitemap()],
+  integrations: [sitemap({ filter: (page) => !page.endsWith("/404/") })],
   redirects: {
     "/analytics": "https://cloud.umami.is/share/fbMpK8dlvWeG41PO",
     "/contact": "https://wa.me/+6281357811710",
   },
-  // TODO: we dont have a domain yet, so this is just a placeholder
-  site: "https://melangkah-studio.com",
+  site: "https://melangkahstudio.site",
   trailingSlash: "always",
 });
